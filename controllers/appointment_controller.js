f4258a469b1fee1b4a3dfcc6655b63a899510861
Will <mitchellspaur@gmail.com>
2022-03-17 20:50:19 +0900

    undergoing testing for how appointment and timeslots are created and interact

--- controllers/appointment_controller.js
+++ controllers/appointment_controller.js
@@ -1,6 +1,8 @@
 const Appointment = require("../models/appointment_model");
 const Timeslot = require("../models/timeslot_model");
 const User = require("../models/user_model");
+const { add } = require("date-fns");
+const Service = require("../models/service_model");
 
 /**
   
@@ -19,21 +21,33 @@ const User = require("../models/user_model");
  */
 
 exports.appointment_post = async (req, res) => {
+  /*
+    Send current time, schedule time, service _id, employee, and user (given by passportJS)
+
+    Pull service for the amount of time slots given
+    Start loop
+      for each service's timeSpan #, increment schedule date by 30mins
+      create and save timeslots
+
+  */
+
   try {
-    const currentTime = Date.now();
-    const { owner, employee, service, slotTime, slotDate, timeSpan } = req.body;
+    const serviceID = req.body.service;
+    const service = Service.findOne({ serviceID });
+    const { employee, slotDateTime, createdAt } = req.body;
     let timeSlots = [];
 
-    for (let i = 1; i <= timeSpan; i++) {
-      let calculatedTime;
+    for (let i = 0; i <= service.timeSpan; i++) {
+      // let calculatedTime = slotTime + (i *= 30);
+
       // Calculate based on whatever NPM module used
-      calculatedTime = slotTime + (i *= 30);
+
+      let calculatedDateTime = add(slotDateTime, { minutes: (i *= 30) });
 
       const newTimeSlot = await new Timeslot({
-        slotTime: calculatedTime,
-        slotDate,
-        createdAt: currentTime,
-        owner,
+        slotDateTime: calculatedDateTime,
+        createdAt,
+        owner: req.user._id,
         employee,
       });
       await newTimeSlot.save();
@@ -43,8 +57,8 @@ exports.appointment_post = async (req, res) => {
     // Might need to merge timeSlots onto appointment? Does mongoose take an array?
     const appointment = new Appointment({
       timeSlots,
-      service,
-      owner,
+      service: service._id,
+      owner: req.user._id,
       employee,
     });
     await appointment.save();
@@ -94,7 +108,7 @@ exports.appointment_patch = async (req, res) => {
   }
   try {
     const currentTime = Date.now();
-    const { owner, employee, service } = req.body;
+    const { owner, employee, service, slotTime, slotDate, timeSpan } = req.body;
 
     const appointment = await Appointment.findOne({
       _id: req.params.id,
--- models/timeslot_model.js
+++ models/timeslot_model.js
@@ -1,27 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const timeslotSchema = new mongoose.Schema(
-  {
-    slotTime: String,
-    slotDate: String,
-    createdAt: Date,
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    },
-    employee: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    },
-    appointment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Appointment'
-    }
-  }
-)
+const timeslotSchema = new mongoose.Schema({
+  slotDateTime: Date,
+  createdAt: Date,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  appointment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Appointment",
+  },
+});
 
-const Timeslot = mongoose.model('Timeslot', timeslotSchema)
+const Timeslot = mongoose.model("Timeslot", timeslotSchema);
 
-module.exports = Timeslot
+module.exports = Timeslot;
